62bb291f8c3821e5a4f2a52e73452fe63d29ca03
jmhirano10 <55772933+jmhirano10@users.noreply.github.com>
2023-10-17 14:06:36 -0400

    Update track.js

--- track.js
+++ track.js
@@ -1,93 +1,92 @@
 class Track{
-    constructor(file,audioContext,trackData,audioData,color){
-        /*Track Info*/
-        this.trackInfo      = document.createElement('div')
-        this.trackName      = document.createElement('p')        
+    constructor(){
+        /*Element Creation*/
+        this.GRAPH_DIV          = document.createElement('div')
+        this.GRAPH_CANVAS       = document.createElement('canvas')
+        this.GRAPH_CTX          = this.GRAPH_CANVAS.getContext('2d')
 
-        /*Audio Visualizer*/
-        this.container      = document.createElement('div')
-        this.graph          = document.createElement('canvas')
-        this.ctx            = this.graph.getContext('2d')
-        this.color          = color
-        this.multiplier     = 60
-        this.audioBuffer    = ''
-        this.sampleRate     = 0
-        this.length         = 0
-        this.duration       = 0
-        this.graphData      = []
+        /*Track Variables*/
+        this.name               = 0
+        this.duration           = 0
+        this.offset             = 0
+        this.multiplier         = 60
+        this.graphData          = []
 
-        /*Audio*/
-        this.srcNode        = audioContext.createBufferSource()
-        this.gainNode       = audioContext.createGain()
-
-        /*Creating HTML Elements*/
-        this.trackInfo.setAttribute('class','track-info')
-        this.trackInfo.appendChild(this.trackName)
-        this.trackName.textContent = file.name
-        this.graph.setAttribute('class','track-graph')
-        this.container.setAttribute('class','track')
-        this.container.appendChild(this.graph)
-        trackData.appendChild(this.trackInfo)
-        audioData.appendChild(this.container)
-
-        /*Set up*/
-        this.createAudioBuffer(file,audioContext)
-        calibrateCanvas(this.graph,this.ctx)
-        this.ctx.translate(0,40)
     }
-
-    /*Loads audioBuffer from given file and audioContext*/
-    async createAudioBuffer(file,audioContext){
+    createAudioGraph(){
+        this.GRAPH_CANVAS.setAttribute('class','track-graph')
+        this.GRAPH_DIV.setAttribute('class','track')
+        let width = 'width:'+this.duration*PIXEL_PER_SEC+'px'
+        console.log(width)
+        this.GRAPH_DIV.setAttribute('style',width)
+        this.GRAPH_DIV.appendChild(this.GRAPH_CANVAS)
+        calibrateCanvas(this.GRAPH_CANVAS,this.GRAPH_CTX)
+        return this.GRAPH_DIV
+    }
+    createSourceNode(gainNode){
+        this.sourceNode = AUDIO_CTX.createBufferSource()
+        this.sourceNode.buffer = this.audioBuffer
+        this.sourceNode.connect(gainNode)
+    }
+    startSourceNode(startTime){
+        let diff = startTime - this.offset
+        if (diff > 0){
+            this.sourceNode.start(0,diff)
+        }
+        else {
+            this.sourceNode.start(Math.abs(diff))
+        }
+    }
+    stopSourceNode(){
+        this.sourceNode.stop()
+    }
+    async createAudioBuffer(file){
         let arrayBuffer     = await file.arrayBuffer()
-        this.audioBuffer    = await audioContext.decodeAudioData(arrayBuffer)
-        this.trackSrc       = this.audioBuffer
-        this.sampleRate     = this.audioBuffer.sampleRate
-        this.length         = this.audioBuffer.length
-        this.updateGraphData()
-        this.srcNode.buffer = this.audioBuffer
-        this.srcNode.connect(this.gainNode)
-        this.gainNode.connect(audioContext.destination)
+        this.audioBuffer    = await AUDIO_CTX.decodeAudioData(arrayBuffer)
     }
-
-    /*Play audio*/
-    playTrack(){
-        this.srcNode.start()
-    }
-
-    /*Pause audio*/
-    pauseTrack(){
-        this.srcNode.stop()
+    calibrateGraph(){
+        calibrateCanvas(this.GRAPH_CANVAS,this.GRAPH_CTX)
+        this.GRAPH_CTX.translate(0,40)
     }
 
     /*Updates the graph data and draws graph*/
-    updateGraphData(){
-        let data = this.audioBuffer.getChannelData(0)
-        this.duration = Math.floor(this.length/this.sampleRate)
-        this.graphData = []
+    createGraphData(){
+        let sampleRate          = this.audioBuffer.sampleRate
+        let length              = this.audioBuffer.length
+        let data                = this.audioBuffer.getChannelData(0)
+        this.duration           = length/sampleRate
+        this.graphData          = []
         for (let i=0; i<=this.duration; i++){
             let sum = 0
-            let time = i*this.sampleRate
-            for (let j=0; j<=this.sampleRate; j++){
+            let time = i*sampleRate
+            for (let j=0; j<=sampleRate; j++){
+                if (time+j >= length){
+                    break
+                }
                 sum += Math.abs(data[time+j])
             }
-            this.graphData.push(sum/this.sampleRate)
-            this.drawLine(i*pixelPerSec,sum/this.sampleRate*this.multiplier)
-        }        
+            this.graphData.push(sum/sampleRate)
+        }
+        
     }
 
     /*Draws graph from graph data*/
-    drawGraph(){
+    drawGraph(color){
+        this.GRAPH_CTX.globalAlpha = 0.2;
+        this.GRAPH_CTX.fillStyle = color;
+        this.GRAPH_CTX.fillRect(0, -40, this.duration*PIXEL_PER_SEC, 80);
+        this.GRAPH_CTX.globalAlpha = 1;
         for (let i=0; i<=this.duration; i++){
-            this.drawLine(i*pixelPerSec,this.graphData[i]*this.multiplier)
+            this.drawGraphBar(i*PIXEL_PER_SEC,this.graphData[i]*this.multiplier,color)
         }
     }
-    drawLine(x,y){
-        this.ctx.beginPath()
-        this.ctx.lineWidth = 2
-        this.ctx.lineCap = "round"
-        this.ctx.strokeStyle = this.color
-        this.ctx.moveTo(x,-y)
-        this.ctx.lineTo(x,y)
-        this.ctx.stroke()
+    drawGraphBar(x,y,color){
+        this.GRAPH_CTX.beginPath()
+        this.GRAPH_CTX.lineWidth = 2
+        this.GRAPH_CTX.lineCap = "round"
+        this.GRAPH_CTX.strokeStyle = color
+        this.GRAPH_CTX.moveTo(x,-y)
+        this.GRAPH_CTX.lineTo(x,y)
+        this.GRAPH_CTX.stroke()
     }
 }
